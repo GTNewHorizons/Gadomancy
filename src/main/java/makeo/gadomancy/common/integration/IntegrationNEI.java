e389ed9c42c8f92c7961e3042220d190eb907d62
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-10-29 22:14:53 +0100

    use NEI api directly to hide items

--- src/main/java/makeo/gadomancy/common/integration/IntegrationNEI.java
+++ src/main/java/makeo/gadomancy/common/integration/IntegrationNEI.java
@@ -1,12 +1,10 @@
 package makeo.gadomancy.common.integration;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import codechicken.nei.api.API;
 import makeo.gadomancy.common.registration.RegisteredBlocks;
 import makeo.gadomancy.common.registration.RegisteredItems;
-import makeo.gadomancy.common.utils.Injector;
 
 /**
  * This class is part of the Gadomancy Mod Gadomancy is Open Source and distributed under the GNU LESSER GENERAL PUBLIC
@@ -21,32 +19,16 @@ public class IntegrationNEI extends IntegrationMod {
         return "NotEnoughItems";
     }
 
-    private Injector nei;
-
     @Override
     protected void doInit() {
-        this.nei = new Injector("codechicken.nei.api.API");
-
-        this.hideItem(new ItemStack(RegisteredItems.itemFakeModIcon));
-        this.hideItem(new ItemStack(RegisteredBlocks.blockStickyJar, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemPackage, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemFakeLootbag, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredBlocks.blockExtendedNodeJar, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemExtendedNodeJar, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemFakeGolemPlacer, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemTransformationFocus, 1, Short.MAX_VALUE));
-        this.hideItem(new ItemStack(RegisteredItems.itemFamiliar_old, 1, Short.MAX_VALUE));
-    }
-
-    private void hideItem(ItemStack item) {
-        this.nei.invokeMethod("hideItem", ItemStack.class, item);
-    }
-
-    public void setItemListEntries(Block block, Iterable<ItemStack> items) {
-        this.setItemListEntries(Item.getItemFromBlock(block), items);
-    }
-
-    public void setItemListEntries(Item item, Iterable<ItemStack> items) {
-        this.nei.invokeMethod("setItemListEntries", new Class[] { Item.class, Iterable.class }, item, items);
+        API.hideItem(new ItemStack(RegisteredItems.itemFakeModIcon));
+        API.hideItem(new ItemStack(RegisteredBlocks.blockStickyJar, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemPackage, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemFakeLootbag, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredBlocks.blockExtendedNodeJar, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemExtendedNodeJar, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemFakeGolemPlacer, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemTransformationFocus, 1, Short.MAX_VALUE));
+        API.hideItem(new ItemStack(RegisteredItems.itemFamiliar_old, 1, Short.MAX_VALUE));
     }
 }
